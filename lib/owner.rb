081815c9d2cfe6a1b079cdd2816ee82a12f25187
dkersten <dkersten1994@gmail.com>
2020-04-02 12:09:07 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
 class Owner
   # code goes here
+
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+
+    Owner.all << self
+  end
+
+  def cats
+    #return all of the specific owner's cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    #return all of the specific owner's dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    #all.count --> all. and self.all. is interchangeable here
+    self.all.count
+  end
+
+  def self.reset_all
+    #all.count --> all. and self.all. is interchangeable here
+    self.all.clear
+  end
 end
