dbf82afee860775412e479b8d3f2f333a8628804
dkersten <dkersten1994@gmail.com>
2020-04-06 11:24:00 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -14,20 +14,6 @@ class Owner
     Owner.all << self
   end
 
-  def cats
-    #return all of the specific owner's cats
-    Cat.all.select do |cat|
-      cat.owner == self
-    end
-  end
-
-  def dogs
-    #return all of the specific owner's dogs
-    Dog.all.select do |dog|
-      dog.owner == self
-    end
-  end
-
   def say_species
     "I am a #{@species}."
   end
@@ -37,12 +23,60 @@ class Owner
   end
 
   def self.count
-    #all.count --> all. and self.all. is interchangeable here
-    self.all.count
+    Owner.all.count
   end
 
   def self.reset_all
-    #all.count --> all. and self.all. is interchangeable here
-    self.all.clear
+    Owner.all.clear
   end
+
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+   end
+
+   def buy_cat(name)
+    Cat.new(name, self)
+   end
+
+   def buy_dog(name)
+    Dog.new(name, self)
+   end
+
+   def walk_dogs
+    self.dogs.each do |dog|
+      dog.mood = "happy"
+    end
+   end
+
+   def feed_cats
+    self.cats.each do |cat| 
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    self.cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    self.dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end
+
+  def list_pets
+    my_cats = self.cats.count
+    my_dogs = self.dogs.count
+    "I have #{my_dogs} dog(s), and #{my_cats} cat(s)."
+  end
+
 end
